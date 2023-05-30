38f7bbbfa7329498a565c2d1c6fe8010dd42fca0
sheaschwenn <sheaschwenn@gmail.com>
2023-05-29 21:56:37 -0700

    styling for responsive design

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -24,10 +24,14 @@
 
 import React from 'react';
 import PortfolioContainer from './components/PortfolioContainer';
+import './styles/App.css'
+
 
 function App(){
   return(
+    <div className= "root">
  <PortfolioContainer/>
+ </div>
   )}
 
 export default App;
